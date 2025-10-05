d76708ff0011c5dd2405bde90cc929436809fec1
marti3 <marcioch.s.m@gmail.com>
2025-10-05 15:24:17 +0200

    Change for regression

--- TEMP_evaluate_on_new_data.py
+++ TEMP_evaluate_on_new_data.py
@@ -7,12 +7,45 @@ from training.VibroNet import VibroNet
 from utils.evaluation.evaluate_classification import evaluate_model
 from utils.config_manager import load_config
 from utils.visualization import plot_confusion_matrix
+from utils.evaluation.evaluate_regression import evaluate_and_plot
 
+"""
+
+def main():
+    config = load_config()
+    model_path = config['training']['model_save_path']
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    print("Loading trained model...")
+    model, checkpoint = load_trained_model(model_path, device)
+
+    print("Creating DataLoader for new data...")
+    checkpoint['config']['data']['data_root'] = config['data']['data_root']
+    checkpoint['config']['data']['annotation_root'] = os.path.join(
+        checkpoint['config']['data']['data_root'], 'annotations'
+    )
+    loader, dataset = create_new_data_loader(checkpoint['config'])
+    print(f"Załadowano {len(dataset)} slice'ów z nowych danych")
+
+
+    print("Evaluating on new data...")
+    accuracy, report, predictions, targets = evaluate_model(model, loader, device)
+
+    # Odzyskanie nazw klas
+    label_to_temp = checkpoint['label_to_temp']
+    class_names = [label_to_temp[i] for i in range(len(label_to_temp))]
+
+    plot_confusion_matrix(targets, predictions, class_names)
+
+    print(f"Accuracy on new data: {accuracy:.4f}")
+    print("\nClassification Report:")
+    print(report)
+"""
 
 def load_trained_model(model_path, device):
     checkpoint = torch.load(model_path, map_location=device)
 
-    model = VibroNet(num_classes=checkpoint['config']['model']['num_classes'])
+    model = VibroNet(mode="regression", num_classes=1)
     model.load_state_dict(checkpoint['model_state_dict'])
     model.to(device)
     model.eval()
@@ -44,7 +77,7 @@ def main():
     model_path = config['training']['model_save_path']
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    print("Loading trained model...")
+    print("Loading trained regression model...")
     model, checkpoint = load_trained_model(model_path, device)
 
     print("Creating DataLoader for new data...")
@@ -55,19 +88,8 @@ def main():
     loader, dataset = create_new_data_loader(checkpoint['config'])
     print(f"Załadowano {len(dataset)} slice'ów z nowych danych")
 
-
-    print("Evaluating on new data...")
-    accuracy, report, predictions, targets = evaluate_model(model, loader, device)
-
-    # Odzyskanie nazw klas
-    label_to_temp = checkpoint['label_to_temp']
-    class_names = [label_to_temp[i] for i in range(len(label_to_temp))]
-
-    plot_confusion_matrix(targets, predictions, class_names)
-
-    print(f"Accuracy on new data: {accuracy:.4f}")
-    print("\nClassification Report:")
-    print(report)
+    print("Evaluating regression model on new data...")
+    evaluate_and_plot(model, loader, device=device)
 
 
 if __name__ == "__main__":
